dccdf647bf039192ad183d3f54ea1d65a008124a
PatriciaOM23 <patricia.olmedo.majano23@gmail.com>
2026-02-16 10:59:25 +0100

    BaseRepository methods already done

--- src/main/java/es/fplumara/dam1/prestamos/model/Prestamo.java
+++ src/main/java/es/fplumara/dam1/prestamos/model/Prestamo.java
@@ -3,9 +3,14 @@ package es.fplumara.dam1.prestamos.model;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
-public class Prestamo {
+public class Prestamo implements Identificable{
     private String id;
     private String idMaterial;
     private String profesor;
     private LocalDate fecha;
+
+    @Override
+    public String getId() {
+        return "";
+    }
 }
--- src/main/java/es/fplumara/dam1/prestamos/repository/BaseRepository.java
+++ src/main/java/es/fplumara/dam1/prestamos/repository/BaseRepository.java
@@ -1,31 +1,32 @@
 package es.fplumara.dam1.prestamos.repository;
 
 import es.fplumara.dam1.prestamos.model.Identificable;
+import es.fplumara.dam1.prestamos.model.Material;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
-public class BaseRepository<T extends Identificable> implements Repository {
+public class BaseRepository<T extends Identificable> implements Repository<T> {
     Map<String,T> datos = new HashMap<>();
     @Override
-    public void save(Identificable element) {
-
+    public void save(T element) {
+    datos.put(element.getId(),element);
     }
 
     @Override
     public Optional findById(String id) {
-        return Optional.empty();
+        return Optional.ofNullable(datos.get(id));
     }
 
     @Override
     public List listAll() {
-        return List.of();
+        return List.of(datos.values());
     }
 
     @Override
     public void delete(String id) {
-
+        datos.remove(id);
     }
 }
