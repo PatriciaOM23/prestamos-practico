1fbcf8d6f434fcaf8ecc305acdebb4865d7de03a
PatriciaOM23 <patricia.olmedo.majano23@gmail.com>
2026-02-16 17:07:10 +0100

    Fallo al resolver merge conflict arreglado

--- src/test/java/es/fplumara/dam1/prestamos/service/PrestamoServiceTest.java
+++ src/test/java/es/fplumara/dam1/prestamos/service/PrestamoServiceTest.java
@@ -1,17 +1,32 @@
 package es.fplumara.dam1.prestamos.service;
 
 
+import es.fplumara.dam1.prestamos.model.EstadoMaterial;
+import es.fplumara.dam1.prestamos.model.Material;
+import es.fplumara.dam1.prestamos.model.Portatil;
+import es.fplumara.dam1.prestamos.model.Prestamo;
 import es.fplumara.dam1.prestamos.repository.BaseRepository;
 import es.fplumara.dam1.prestamos.repository.MaterialRepositoryImpl;
 import es.fplumara.dam1.prestamos.repository.PrestamoRepositoryImpl;
 import es.fplumara.dam1.prestamos.repository.Repository;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.time.LocalDate;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
 @ExtendWith(MockitoExtension.class)
 class PrestamosServiceTest {
+    @InjectMocks
+    PrestamoService prestamoService;
     @Mock
     PrestamoRepositoryImpl prestamoRepository;
     @Mock
@@ -31,7 +46,7 @@ class PrestamosServiceTest {
     void crearPrestamo_ok_cambiaEstado_y_guarda(){
         Material material = new Portatil("1","laptop hp", EstadoMaterial.DISPONIBLE);
         when(materialRepository.findById("1")).thenReturn(Optional.of(material));
-        Prestamo prestamo = prestamoService.crearPrestamo(material.getId(),"Ivan",LocalDate.now());
+        Prestamo prestamo = prestamoService.crearPrestamo(material.getId(),"Ivan", LocalDate.now());
         verify(prestamoRepository).save(any(Prestamo.class));
         assertEquals(EstadoMaterial.PRESTADO,material.getEstadoMaterial());
     }
