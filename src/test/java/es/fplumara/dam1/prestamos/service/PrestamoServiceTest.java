a5b49bdc71b5e9d4b5a1dc889957a1f0228e5469
PatriciaOM23 <patricia.olmedo.majano23@gmail.com>
2026-02-18 11:29:28 +0100

    tests running already

--- src/test/java/es/fplumara/dam1/prestamos/service/PrestamoServiceTest.java
+++ src/test/java/es/fplumara/dam1/prestamos/service/PrestamoServiceTest.java
@@ -34,15 +34,6 @@ class PrestamosServiceTest {
     @Mock
     MaterialRepositoryImpl materialRepository;
 
-
-    // TODO (alumnos): añadir JUnit 5 y Mockito en el pom.xml y completar:
-    //
-    // - crearPrestamo_ok_cambiaEstado_y_guarda()
-    // - crearPrestamo_materialNoExiste_lanzaNoEncontrado()
-    // - crearPrestamo_materialNoDisponible_lanzaMaterialNoDisponible()
-    // - devolverMaterial_ok_cambiaADisponible()
-
-    // Requisito: usar mocks de repositorios y verify(...)
     @Test
     void crearPrestamo_ok_cambiaEstado_y_guarda(){
         Material material = new Portatil("1","laptop hp", EstadoMaterial.DISPONIBLE);
